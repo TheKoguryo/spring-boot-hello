5b9b642835be2ff4b4ac1c4ce80535287aa57e87
dhlee <dhlee@221.142.245.107>
2019-07-01 15:50:32 +0900

    ..

--- ci/build.sh
+++ ci/build.sh
@@ -12,6 +12,6 @@ ls -la target/
 
 mkdir -p /tmp/build/put/build-output
 
-cp target/*.jar /tmp/build/build-output/.
+cp target/*.jar /tmp/build/put/build-output/.
 
-ls -la /tmp/build/build-output/
+ls -la /tmp/build/put/build-output/
